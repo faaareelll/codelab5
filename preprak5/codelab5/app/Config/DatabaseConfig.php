328774a2c0e7bda8ff82829f20cade0c14edbd80
faaareelll <128385693+faaareelll@users.noreply.github.com>
2023-11-28 21:20:48 +0700

    Add files via upload

---
+++ preprak5/codelab5/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig 
+{
+    // Setting konfigurasi database kalian
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "m5";
+    public $port = 3306;
+}
